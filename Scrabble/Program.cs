bbac207800c9e83298b3156e6c994239b362681e
Student <example@example.com>
2020-03-05 14:27:07 -0800

    added UI

--- Scrabble/Program.cs
+++ Scrabble/Program.cs
@@ -7,8 +7,11 @@ namespace Game
   {
     public static void Main()
     {
-     Console.WriteLine();
+     Console.WriteLine("Hello. Tell me a word and I'll tell you secret");
+     string response = Console.ReadLine();
+     Word newResponse = new Word(response);
+     int score = newResponse.GetScore();
+     Console.WriteLine("your score is " + score);
     }
   }
-
 }

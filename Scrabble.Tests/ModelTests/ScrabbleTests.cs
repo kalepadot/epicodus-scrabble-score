f3cdad62f75fab520c58ae16801d5af1e73b514b
Student <example@example.com>
2020-03-05 13:20:07 -0800

    pushing to master

--- Scrabble.Tests/ModelTests/ScrabbleTests.cs
+++ Scrabble.Tests/ModelTests/ScrabbleTests.cs
@@ -8,10 +8,91 @@ namespace Game.Tests
   public class MakeFilesTests
   {
     [TestMethod]
-    public void MethodName_WhatItDoes_ReturnObject()
+    public void WordConstructor_CreateNewInstance_Word()
     {
-
+      // Arrange
+      string scrabbleWord = "Haru";
+      Word newWord = new Word(scrabbleWord);
+      // Act
+      // Assert
+      Assert.AreEqual(typeof(Word), newWord.GetType());
     }
-
+    [TestMethod]
+    public void GetScore_CalculateScore_Int()
+    {
+      // arrange
+      string scrabbleWord = "ae";
+      Word newWord = new Word(scrabbleWord);
+      // act
+      int score = newWord.GetScore();
+      // assert
+      Assert.AreEqual(2, score);
+    }
+    [TestMethod]
+    public void GetScore_Calculate2Points_Int()
+    {
+      // arrange
+      string scrabbleWord = "dog";
+      Word newWord = new Word(scrabbleWord);
+      // act
+      int score = newWord.GetScore();
+      // assert
+      Assert.AreEqual(5, score);
+    }
+      [TestMethod]
+    public void GetScore_Calculate3Points_Int()
+    {
+      // arrange
+      string scrabbleWord = "camp";
+      Word newWord = new Word(scrabbleWord);
+      // act
+      int score = newWord.GetScore();
+      // assert
+      Assert.AreEqual(10, score);
+    }
+    [TestMethod]
+    public void GetScore_Calculate4Points_Int()
+    {
+      // arrange
+      string scrabbleWord = "four";
+      Word newWord = new Word(scrabbleWord);
+      // act
+      int score = newWord.GetScore();
+      // assert
+      Assert.AreEqual(7,score);
+    }
+    [TestMethod]
+    public void GetScore_Calculate5Points_Int()
+    {
+            // arrange
+      string scrabbleWord = "kool";
+      Word newWord = new Word(scrabbleWord);
+      // act
+      int score = newWord.GetScore();
+      // assert
+      Assert.AreEqual(8,score);
+    }
+      [TestMethod]
+      public void GetScore_Calculate8Points_Int()
+    {
+            // arrange
+      string scrabbleWord = "jeremy";
+      Word newWord = new Word(scrabbleWord);
+      // act
+      int score = newWord.GetScore();
+      // assert
+      Assert.AreEqual(18,score);
+  }
+      [TestMethod]
+      public void GetScore_Calculate10Points_Int()
+    {
+            // arrange
+      string scrabbleWord = "zoro";
+      Word newWord = new Word(scrabbleWord);
+      // act
+      int score = newWord.GetScore();
+      // assert
+      Assert.AreEqual(13,score);
+    }
   }
 }

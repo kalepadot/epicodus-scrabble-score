61e8c40e96752bbbd851660704496c9f097c0bcb
Student <example@example.com>
2020-03-05 14:02:48 -0800

    dry code with dictionary

--- Scrabble/Models/Scrabble.cs
+++ Scrabble/Models/Scrabble.cs
@@ -1,58 +1,32 @@
 using System;
+using System.Collections.Generic;
 
 namespace Game.Models
 {
   public class Word
   {
     public string ScrabbleWord { get; set; }
+    public Dictionary<string, int> ScoreKey = new Dictionary<string, int>(){{"aeioulnrst", 1},{"dg", 2},{"bcmp", 3},{"fhvwy", 4},{"k", 5},{"jx", 8},{"qz", 10}};
+   
     
     public Word(string scrabbleWord)
     { 
       ScrabbleWord = scrabbleWord;
-      // Add Score Counter that method adds up via GetScore method
     }
 
     public int GetScore()
     {
-      string onePoint = "aeioulnrst";
-      string twoPoint = "dg";
-      string threePoint = "bcmp";
-      string fourPoint = "fhvwy";
-      string fivePoint = "k";
-      string eightPoint = "jx";
-      string tenPoint = "qz";
       int scoreCounter = 0;
       // string newWord = Scrabbleword.ToLower();
       char[] splitString = ScrabbleWord.ToCharArray();
       for(int i = 0; i < splitString.Length; i++)
-        { 
-        if (onePoint.Contains(splitString[i]))
+      { 
+        foreach(KeyValuePair<string, int> item in ScoreKey)
         {
-        scoreCounter++;
-        }
-        else if(twoPoint.Contains(splitString[i]))
-        {
-          scoreCounter+=2;
-        }
-        else if(threePoint.Contains(splitString[i]))
-        {
-          scoreCounter+=3;
-        }
-        else if(fourPoint.Contains(splitString[i]))
-        {
-          scoreCounter+=4;
-        }
-          else if(fivePoint.Contains(splitString[i]))
-        {
-          scoreCounter+=5;
-        }
-          else if(eightPoint.Contains(splitString[i]))
-        {
-          scoreCounter+=8;
-        }
-          else if(tenPoint.Contains(splitString[i]))
-        {
-          scoreCounter+=10;
+          if (item.Key.Contains(splitString[i]))
+          {
+            scoreCounter += item.Value;
+          }
         }
       }
       return scoreCounter;
